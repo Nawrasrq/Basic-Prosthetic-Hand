b3dd1e177a6f3708bf4948e731a1abc996906244
Nawras Rawas Qalaji <nawrasqalaji@gmail.com>
2024-05-07 15:09:07 -0400

    Removed actuator speed, hand moves at max speed
    Removed a feature where actuators would move at the speed equal to the difference between previous and current EMG readings to better simulate natural hand movements
    - Beyond the scope of the current project and added unnecessary complexity (Unnecessary feature, made using the hand more complicated)
    - Hand only really needs to either be opened or closed

--- Prosthetic_Hand.ino
+++ Prosthetic_Hand.ino
@@ -3,13 +3,10 @@
 #define ACTUATORS_IN2 5                     // Input 2 for the actuator driver 
 #define EMG_PIN A0                          // EMG sensor ping
 #define EMG_THRESHOLD 20.46                 // Threshold to close hand before converting to milli volts
-#define EMG_TO_ACTUATOR_SPEED 255/1023      // Convert EMG to actuator speed
 #define EMG_TO_MV 5/1023*1000               // Convert EMG to milli volts
-#define AMPLIFY_SPEED 12                    // Hand moves slow, amplify it by hardcoded value
 
 double EMG_Curr = 0.0;                      // Current EMG Reading
 double EMG_Prev = 0.0;                      // Previous EMG reading
-double actuator_speed = 0.0;                // absolute val of the difference between curr and prev EMG vals
 
 void setup() {
   // Set pins
@@ -32,23 +29,16 @@ void loop() {
   EMG_Prev = EMG_Curr;
   EMG_Curr = analogRead(EMG_PIN);
 
-  // Convert EMG to Actuator speed and amplify its value (base actuator speed range: 0 to 255)
-  actuator_speed = abs(EMG_Prev - EMG_Curr) * EMG_TO_ACTUATOR_SPEED * AMPLIFY_SPEED;
-
-  Serial.println("EMG Value: " + String(EMG_Curr * EMG_TO_MV) + "mV\n Actuator Speed: " + String(actuator_speed));
+  Serial.println("EMG Value: " + String(EMG_Curr * EMG_TO_MV) + "mV");
 
   // Close hand
   if(EMG_Curr >= EMG_THRESHOLD){
-    digitalWrite(ACTUATOR_DRIVER, actuator_speed);
-
     analogWrite(ACTUATORS_IN1, HIGH);
     analogWrite(ACTUATORS_IN2, LOW);
   }
 
   // Open
   else {
-    digitalWrite(ACTUATOR_DRIVER, actuator_speed);
-
     analogWrite(ACTUATORS_IN1, LOW);
     analogWrite(ACTUATORS_IN2, HIGH);
   }
